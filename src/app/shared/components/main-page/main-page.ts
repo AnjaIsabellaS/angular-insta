d513945395f5c401949ca4cdcaf9eac65a757d37
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-07-22 23:31:28 +0200

    feat: interface added in main-page.ts

--- src/app/shared/components/main-page/main-page.ts
+++ src/app/shared/components/main-page/main-page.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
 
+interface Post {
+  name: string;
+  img: string;
+  Likes: number;
+}
+
 @Component({
   selector: 'app-main-page',
   imports: [],
   templateUrl: './main-page.html',
   styleUrl: './main-page.scss',
 })
-export class MainPage {}
+export class MainPage {
+   posts: Post[] = [
+    { name: 'Anja', img: 'images/banana.jpg', Likes: 95 },
+    { name: 'Charlotte', img: 'images/orange.jpg', Likes: 3 },
+    { name: 'Lisa', img: 'images/currant.jpg', Likes: 12 },
+  ];
+}
