98a74fdd81307dc3b9c246739264b7912e7192dd
Anja-Isabella Schulz <anja-isabella.schulz@gmx.de>
2026-07-29 12:58:36 +0200

    feat: add comments array to posts and render test comments

--- src/app/shared/components/main-page/main-page.ts
+++ src/app/shared/components/main-page/main-page.ts
@@ -10,8 +10,29 @@ import { SinglePost } from './single-post/single-post';
 })
 export class MainPage {
   posts: Post[] = [
-    { name: 'Anja', img: 'images/banana.jpg', Likes: 95, isLiked: false },
-    { name: 'Charlotte', img: 'images/orange.jpg', Likes: 3, isLiked: false },
-    { name: 'Lisa', img: 'images/currant.jpg', Likes: 12, isLiked: false },
-  ];
+  {
+    name: 'Anja',
+    img: 'images/banana.jpg',
+    Likes: 95,
+    isLiked: false,
+    comments: [
+      { id: 1, author: 'Charlotte', text: 'Schöne Banane!' },
+      { id: 2, author: 'Lisa', text: 'Nice' },
+    ],
+  },
+  {
+    name: 'Charlotte',
+    img: 'images/orange.jpg',
+    Likes: 3,
+    isLiked: false,
+    comments: [{ id: 3, author: 'Anja', text: 'Yummy!' }],
+  },
+  {
+    name: 'Lisa',
+    img: 'images/currant.jpg',
+    Likes: 12,
+    isLiked: false,
+    comments: [],
+  },
+];
 }
--- src/app/shared/interfaces/post.ts
+++ src/app/shared/interfaces/post.ts
@@ -3,4 +3,11 @@ export interface Post {
   img: string;
   Likes: number;
   isLiked: boolean;
+  comments: Comment[];
+}
+
+export interface Comment {
+  id: number;
+  author: string;
+  text: string;
 }
